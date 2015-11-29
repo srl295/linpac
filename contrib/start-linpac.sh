178ede8413269b5e58058a03b36016fe37f0b4f5
David Ranch <linpac@trinnet.net>
2015-11-29 11:33:40 -0800

    better grepping, disabled logging for Ncurses UI

--- contrib/start-linpac.sh
+++ contrib/start-linpac.sh
@@ -3,6 +3,7 @@
 # /usr/local/sbin/start-linpac.sh
 # dranch@arrl.net
 
+# 11/29/15 - Removed screen logging as it doesn't work well for Ncurses UIs
 # 12/11/14 - Enabled screen logging to a specific file
 # 08/30/14 - disabled screen logging
 # 02/06/14 - Added screen loggin
@@ -16,12 +17,12 @@
 #
 #   Don't enable screen logging (-L) as it just creates ANSI trash in the logs
 
-if [ -n "`ps aux | grep -i screen | grep -i linpac`" ]; then
+if [ -n "`ps aux | grep -v grep | grep -i screen | grep -i linpac`" ]; then
    echo -e "\nLinpac already running under Screen.  Resuming"
    screen -d -r linpac
   else
    echo -e "\nPrevious Linpac not found.  Starting"
    date > /var/log/linpac.log
-   screen -c $HOME/.screenrc-svxlink -S linpac sudo linpac
+   screen -S linpac sudo linpac
 fi
 
